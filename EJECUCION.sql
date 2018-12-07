841e8ab7a9e4a55f3593d38ef1ed6b2ff778b1b6
jorvilaco <jorvilaco@gmail.com>
2018-12-07 12:09:58 +0100

    Update EJECUCION.sql

--- EJECUCION.sql
+++ EJECUCION.sql
@@ -31,9 +31,13 @@ BEGIN
   
   PRUEBAS_VEHICULOS.INICIALIZAR;
   DBMS_OUTPUT.PUT_LINE(CR_LF||'Pruebas sobre tabla de Vehiculos (CÓdigo Prueba/AcciÓn/T�tulo Prueba/Punto de Sincronismo esperado-->Resultado!)'||CR_LF);
-  PRUEBAS_VEHICULOS.INSERTAR  ('Tpro-01/Insert-"Vehiculos=V1"                               /Commit  --> ','2356JBK',TO_DATE(SYSDATE),
+  PRUEBAS_VEHICULOS.INSERTAR  ('Tpro-01/Insert-"Vehiculos=Prueba"                           /Commit  --> ','2286JBK',TO_DATE(SYSDATE),
   'Citroen C3 HDI Negro','Retrovisor exterior ......',10040,1,null,null,true);
   cod_veh := seq_vehiculos.currval;
+  PRUEBAS_VEHICULOS.INSERTAR  ('Tpro-01/Insert-"Vehiculos=V1"                               /Commit  --> ','2356JBK',TO_DATE(SYSDATE),
+  'Citroen C3 HDI Negro','Retrovisor exterior ......',10040,1,null,null,true);
+  PRUEBAS_VEHICULOS.INSERTAR  ('Tpro-01/Insert-"Vehiculos=V2"                               /Commit  --> ','2386JBK',TO_DATE(SYSDATE),
+  'Citroen C3 HDI Blanco','Retrovisor exterior ......',9040,1,null,null,true);
   PRUEBAS_VEHICULOS.INSERTAR  ('Tpro-02/Insert/"Vehiculos.MatriculoDuplicada=2356JBK"       /Rollback--> ','2356JBK',TO_DATE(SYSDATE), 
   'Citroen C3 HDI Negro','Retrovisor exterior ......',10040,1,null,null,false);
   PRUEBAS_VEHICULOS.INSERTAR  ('Tpro-02/Insert/"Vehiculos.Matricula=Null"                   /Rollback--> ',null,TO_DATE(SYSDATE), 
@@ -49,6 +53,7 @@ BEGIN
   PRUEBAS_VEHICULOS.ACTUALIZAR('Tpro-03/Update/"Vehiculos=2356JBM"                          /Commit  --> ',cod_veh,'2356JBM',TO_DATE(SYSDATE),
   'Citroen C3 HDI Negro','Retrovisor exterior ......',10040,1,null,null,true);
   --eliminar_vehiculos(cod_veh);
-  PRUEBAS_VEHICULOS.ELIMINAR  ('Tpro-05/Delete/"Vehiculos=V1"                               /Commit  --> ',cod_veh,true);
+  PRUEBAS_VEHICULOS.ELIMINAR  ('Tpro-05/Delete/"Vehiculos=Prueba"                           /Commit  --> ',cod_veh,true);
+  
   
   END;
