e448c7abc863b2efe3487f07bb732300901a1b32
jorvilaco <jorvilaco@gmail.com>
2018-12-07 12:23:14 +0100

    Update EJECUCION.sql

--- EJECUCION.sql
+++ EJECUCION.sql
@@ -10,18 +10,17 @@ BEGIN
         PRUEBAS DE LAS OPERACIONES SOBRE LA TABLA TIPOPROPIEDADES 
   **********************************************************************/
   PRUEBAS_TIPOPROPIEDADES.INICIALIZAR;
-  DBMS_OUTPUT.PUT_LINE(CR_LF||'Pruebas sobre tabla de TipoPropiedades (C�digo Prueba/Acci�n/T�tulo Prueba/Punto de Sincronismo esperado-->Resultado!)'||CR_LF);
-  PRUEBAS_TIPOPROPIEDADES.INSERTAR  ('Tpro-01/Insert-"TipoPropiedades=Llanta"                         /Commit  --> ','Llanta',true);
+  DBMS_OUTPUT.PUT_LINE(CR_LF||'Pruebas sobre tabla de TipoPropiedades (Código Prueba/Acción/T�tulo Prueba/Punto de Sincronismo esperado-->Resultado!)'||CR_LF);
+  PRUEBAS_TIPOPROPIEDADES.INSERTAR  ('Tpro-01/Insert-"TipoPropiedades=Llanta"                /Commit  --> ','Llanta',true);
   cod_tpro := seq_tipopropiedades.currval;
-  PRUEBAS_TIPOPROPIEDADES.INSERTAR  ('Tpro-02/Insert/"TipoPropiedades=Null"                              /Rollback--> ',null,false);
-  PRUEBAS_TIPOPROPIEDADES.ACTUALIZAR('Tpro-03/Update/"TipoPropiedades=Llantas"                        /Commit  --> ',cod_tpro,'Llantas',true);
-  PRUEBAS_TIPOPROPIEDADES.ACTUALIZAR('Tpro-04/Update/"TipoPropiedades=Null"                              /Rollback--> ',cod_tpro,null,false);
-  PRUEBAS_TIPOPROPIEDADES.ELIMINAR  ('Tpro-05/Delete/"TipoPropiedades=Llantas"                        /Commit  --> ',cod_tpro,true);
-  insertar_tipo_propiedades('Puertas');
-  insertar_tipo_propiedades('Color');
-  insertar_tipo_propiedades('Combustible');
-  insertar_tipo_propiedades('Etiqueta Eficiencia');
-  
+  PRUEBAS_TIPOPROPIEDADES.INSERTAR  ('Tpro-01/Insert-"TipoPropiedades=T1"                    /Commit  --> ','Color Carrocería',true);
+  PRUEBAS_TIPOPROPIEDADES.INSERTAR  ('Tpro-01/Insert-"TipoPropiedades=T2"                    /Commit  --> ','Nº Puertas',true);
+  PRUEBAS_TIPOPROPIEDADES.INSERTAR  ('Tpro-01/Insert-"TipoPropiedades=T3"                    /Commit  --> ','Cámra Trasera',true);
+  PRUEBAS_TIPOPROPIEDADES.INSERTAR  ('Tpro-02/Insert/"TipoPropiedades=Null"                  /Rollback--> ',null,false);
+  PRUEBAS_TIPOPROPIEDADES.ACTUALIZAR('Tpro-03/Update/"TipoPropiedades=Llantas"               /Commit  --> ',cod_tpro,'Llantas',true);
+  PRUEBAS_TIPOPROPIEDADES.ACTUALIZAR('Tpro-04/Update/"TipoPropiedades=Null"                  /Rollback--> ',cod_tpro,null,false);
+  PRUEBAS_TIPOPROPIEDADES.ELIMINAR  ('Tpro-05/Delete/"TipoPropiedades=Llantas"               /Commit  --> ',cod_tpro,true);
+ 
 
   
   /*********************************************************************
