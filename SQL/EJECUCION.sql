44ee9895a9c2c3017dc4217762f4a337380bc571
jorvilaco <44461404+jorvilaco@users.noreply.github.com>
2019-03-06 14:49:07 +0100

    Update EJECUCION.sql

--- SQL/EJECUCION.sql
+++ SQL/EJECUCION.sql
@@ -99,9 +99,9 @@ BEGIN
   PRUEBAS_VEHICULOS.INSERTAR  ('Veh-02/Insert-"Vehiculos=V1"                            /Commit  --> ','1356JBK',TO_DATE('2017-12-20', 'YYYY-MM-DD'),
   'Citroen C3 HDI Negro','Retrovisor exterior ......',10040,1,1,null,true);
   select id_conces into prueba_conces from vehiculos where matricula = '1356JBK';
-  PRUEBAS_EMPLEADOS.INSERTAR('Emp-01/Insert-"Empleados=E2"                              /Commit -->','Rosa Márquez','Jefa de Marketing ','rosmar','1904m', '48592056B', 1,true);
-  PRUEBAS_EMPLEADOS.INSERTAR('Emp-02/Insert-"Empleados=E3"                              /Commit -->','María Martínez ','Jefa de Ventas ','marimar','mmz1889', '01724748R', 1 ,true);
-  PRUEBAS_EMPLEADOS.INSERTAR('Emp-02/Insert-"Empleados=E3"                              /Commit -->','María Tom ','Jefa de Ventas ','mari','mmz18', '01724748V', 1 ,true);
+  PRUEBAS_EMPLEADOS.INSERTAR('Emp-01/Insert-"Empleados=E2"                              /Commit -->','Rosa Márquez','administrativo','rosmar','1904m', '48592056B', 1,true);
+  PRUEBAS_EMPLEADOS.INSERTAR('Emp-02/Insert-"Empleados=E3"                              /Commit -->','María Martínez ','vendedor','marimar','mmz1889', '01724748R', 1 ,true);
+  PRUEBAS_EMPLEADOS.INSERTAR('Emp-02/Insert-"Empleados=E3"                              /Commit -->','María Tom ','vendedor','mari','mmz18', '01724748V', 1 ,true);
   PRUEBAS_CONCESIONARIO.ACTUALIZAR('Con-9/Update-"Concesionario= prueba"                    /Commit-->',cod_conces,'Con','calle',756493498,'prueba@prueba.com',1,true);
   PRUEBAS_CONCESIONARIO.ELIMINAR('Con-10/Delete-"Concesionario = prueba"                  /Commit-->',cod_conces,true);
   select id_conces into prueba_conces2 from vehiculos where matricula = '1356JBK';
